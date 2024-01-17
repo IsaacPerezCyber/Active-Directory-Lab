de2de825e1304f86b093fa8917b0891b3fbdf1b8
Isaac Perez <67014769+IsaacPerezCyber@users.noreply.github.com>
2024-01-17 11:42:32 -0800

    Add files via upload

---
+++ AD_Create_Users.ps1
@@ -0,0 +1,25 @@
+﻿#Setting password the same for all new users. will need to change in AD. 
+$PASSWORD_FOR_USERS   = "Password1"
+#Getting name from the names text file
+$USER_FIRST_LAST_LIST = Get-Content .\names.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
